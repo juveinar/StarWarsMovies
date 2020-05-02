efd8e670e1b69b6ac138abc9cfd54eea41919833
Davinchi5 <63528072+Davinchi5@users.noreply.github.com>
2020-05-02 17:42:50 -0500

    Update app-routing.module.ts
    Adicionando la rutas de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { POLITECNICOComponent } from './politecnico/politecnico.component';
 import { CharacterComponent } from './character/character.component';
 import { MoviesComponent } from './movies/movies.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { PlanetasComponent } from './planetas/planetas.component';
+
 
 const routes: Routes = [
   { path: 'home', component: POLITECNICOComponent },
   { path: 'character', component: CharacterComponent },
   { path: 'movies', component: MoviesComponent  },
-
+  {path: 'pelis', component:PeliculasComponent},
+    {path: 'person', component:PersonajesComponent},  
+  {path: 'planet', component:PlanetasComponent}  
 ];
 
 @NgModule({
